a54315b98d2c3ddc0b00eb455ae7b85e4c5be6b9
amiraba <a.bachour.tn@gmail.com>
2017-04-17 23:44:21 +0200

    Update beacons.js

--- routes/beacons.js
+++ routes/beacons.js
@@ -44,46 +44,4 @@ router.post('/beacon', function(req, res, next){
     }
     console.log("POST request...");
 });
-/*
-//Delete task
-router.delete('/tasks/:id', function(req, res, next){
-    db.tasks.remove(
-        {_id: mongojs.ObjectId(req.params.id)},
-        function(err, task){
-            if (err){
-                res.send(err);
-            }
-            res.json(task);
-        });
-});
-
-//Update task
-router.put('/tasks/:id', function(req, res, next){
-    var task=req.body;
-    var updTask={};
-
-    if (task.isDone){
-        updTask.isDone=task.isDone;
-    }
-    if (task.title){
-        updTask.title=task.title;
-    }
-    if (!updTask){
-        res.status(400);
-        res.json({
-            "error": "Bad Data"
-        });
-    }
-    db.tasks.update(
-        {_id: mongojs.ObjectId(req.params.id)},
-        updTask,
-        {},
-        function(err, task){
-            if (err){
-                res.send(err);
-            }
-            res.json(task);
-        });
-});
-*/
 module.exports = router;
